6c3a221d872ab7472c1a80c104d81bcabf057525
Lueil <782995985@qq.com>
2023-11-25 21:18:49 +0800

    exp 23

---
+++ Vue基础练习1/task3-data.js
@@ -0,0 +1,26 @@
+[
+  {
+    id: 1,
+    title: "Java无难事",
+    price: 188,
+    count: 1,
+  },
+  {
+    id: 2,
+    title: "VC++深入详解",
+    price: 168,
+    count: 1,
+  },
+  {
+    id: 3,
+    title: "Servlet/JSP深入详解",
+    price: 139,
+    count: 1,
+  },
+  {
+    id: 4,
+    title: "Vue.js 3.x从入门到实战",
+    price: 88,
+    count: 1,
+  },
+],
